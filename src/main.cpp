2e8f6a2ac15235d48bff6ddc8877ab4ed12980cc
maxnibler <max.nibler@gmail.com>
2021-03-01 19:18:44 +0000

    Init project

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//Maximillian H Nibler
+//max.nibler@gmail.com
+//https://github.com/maxnibler/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  cout << "Hello World" << endl;
+  return 0;
+}
